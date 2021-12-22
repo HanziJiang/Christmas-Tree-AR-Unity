974a4b5b9a5421e2c6b5c1b809672647871243ae
Clara Guo <guoguo.clara@gmail.com>
2021-12-22 13:36:58 -0500

    add TouchManager script and remove `scale` code from TapToPlace

--- Assets/Scripts/TapToPlace.cs
+++ Assets/Scripts/TapToPlace.cs
@@ -15,9 +15,6 @@ public class TapToPlace : MonoBehaviour
     private Pose placementPose;
     private Camera currentCamera;
 
-    private float initialDistance;
-    private Vector3 initialScale;
-
     void Start()
     {
         arRaycastManager = FindObjectOfType<ARRaycastManager>();
@@ -35,46 +32,6 @@ public class TapToPlace : MonoBehaviour
             print("Placing AR object...");
             ARPlaceObject();  // at the moment this just spawns the gameobject
         }
-
-        // scale using pinch involves two touches
-        // we need to count both the touches, store it somewhere, measure the distance between pinch 
-        // and scale gameobject depending on the pinch distance
-        // we also need to ignore if the pinch distance is small (cases where two touches are registered accidentally)
-
-        if(placedObject != null && Input.touchCount == 2)
-        {
-            var touchZero = Input.GetTouch(0);
-            var touchOne = Input.GetTouch(1);
-
-            // if any one of touchzero or touchOne is cancelled or maybe ended then do nothing
-            if(touchZero.phase == TouchPhase.Ended || touchZero.phase == TouchPhase.Canceled ||
-                touchOne.phase == TouchPhase.Ended || touchOne.phase == TouchPhase.Canceled)
-            {
-                return; // do nothing
-            }
-
-            if(touchZero.phase == TouchPhase.Began || touchOne.phase == TouchPhase.Began)
-            {
-                initialDistance = Vector2.Distance(touchZero.position, touchOne.position);
-                initialScale = placedObject.transform.localScale;
-                Debug.Log("Initial Distance: " + initialDistance + "GameObject Name: "
-                    + placedObject.name); // Just to check in console
-            }
-            else // if touch is moved
-            {
-               var currentDistance = Vector2.Distance(touchZero.position, touchOne.position);
-
-                //if accidentally touched or pinch movement is very very small
-                if (Mathf.Approximately(initialDistance, 0))
-                {
-                    return; // do nothing if it can be ignored where initial distance is very close to zero
-                }
-
-               var factor = currentDistance / initialDistance;
-                placedObject.transform.localScale = initialScale * factor; // scale multiplied by the factor we calculated
-            }
-        }
-        
     }
     void ARPlaceObject()
     {
